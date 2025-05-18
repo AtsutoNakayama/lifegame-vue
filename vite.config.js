b832a3ea012e29ba4634a32998ae5a6b8f7dc3ae
nakayama <atsuto010707@yahoo.co.jp>
2025-05-18 21:29:35 +0900

    スペルミス修正

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  bbase: '/lifegame-vue/', // GitHub Pages用にbaseパスを指定
+  base: '/lifegame-vue/', // GitHub Pages用にbaseパスを指定
   plugins: [vue()],
 })
